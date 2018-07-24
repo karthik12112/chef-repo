68ae6647603e3659ea13696a7c21100d718f6119
saikrishnaprasad02 <35852483+saikrishnaprasad02@users.noreply.github.com>
2018-07-23 20:09:02 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "terraform-vault" {
   subnet_id              = "${aws_subnet.public.id}"
   vpc_security_group_ids = "${aws_security_group.sgroup.id}"
   user_data              = "${var.user_data}"
-
+  } 
   # EC2 instance must be in a public subnet
   subnet_id = "${aws_subnet.public.id}"
 
